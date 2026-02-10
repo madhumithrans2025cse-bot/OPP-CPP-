f41b08fc6e6160f4a12e0ea97c2745778f34aa87
MADHU MITHRAN S <madhumithran.s2025cse@sece.ac.in>
2026-02-10 20:16:11 +0530

    Update problem  9.cpp

--- LAB 2/problem  9.cpp
+++ LAB 2/problem  9.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-int main()
-{
-    
-    const int MAX_MARKS = 100;
-    const int SUBJECTS = 5;
+int main() {
+    const double URGENT_THRESHOLD = 20.0;
+    double stock = 100, sales[7], total;
+    int choice;
 
-    
-    short students = 45;
-    long collegeCode = 202600123L;
-    char section = 'A';
+    do {
+        total = 0;
 
-    
-    int marks[SUBJECTS];
-    int total = 0;
-    double average;
-    bool pass;
 
-    
-    cout << "Enter marks for 5 subjects:\n";
-    for (int i = 0; i < SUBJECTS; i++)
-    {
-        cin >> marks[i];
-        total += marks[i];
-    }
+        for (int i = 0; i < 7; i++) {
+            cout << "Enter sales for day " << i + 1 << ": ";
+            cin >> sales[i];
+            total += sales[i];
+        }
 
-    average = total / (double)SUBJECTS;
-    pass = (average >= 50.0);
+        stock -= total;
+        cout << "Current Stock: " << stock << endl;
 
-    cout<<setfill('')<<setw(10)<<""<<"I CSE EXAM RESULTS"<<setfill('')<<setw(10)<<""<<endl;
-    cout << fixed << setprecision(2);
-    cout << "\nCollege Code: " << collegeCode << endl;
-    cout << "Section: " << section << endl;
-    cout << "Total Marks: " << total << "/" << (SUBJECTS * MAX_MARKS) << endl;
-    cout << "Average: " << average << "%" << endl;
-    cout << "Result: " << (pass ? "PASS" : "FAIL") << endl;
+        
+        if (stock < 50) {
+            cout << "Reorder needed.\n";
+
+            
+            switch (stock < URGENT_THRESHOLD) {
+                case 1:
+                    cout << "Vendor1 selected (Fast delivery)\n";
+                    stock += 60;
+                    break;
+                case 0:
+                    cout << "Vendor2 selected (Cheap)\n";
+                    stock += 40;
+                    break;
+            }
+        } else {
+            cout << "Stock sufficient.\n";
+        }
+
+        cout << "Projected Stock: " << stock << endl;
+
+        cout << "\nContinue? (1-Yes / 0-No): ";
+        cin >> choice;
+
+    } while (choice == 1);
 
     return 0;
 }
